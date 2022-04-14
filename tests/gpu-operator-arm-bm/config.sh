e9d8b54dbad5ce63b161a691b4ba438e4d2158a5
Costin Gamenț <costin.gament@gmail.com>
2022-04-14 12:38:18 +0200

    Add proper kubeconfig path for the CI

--- tests/gpu-operator-arm-bm/config.sh
+++ tests/gpu-operator-arm-bm/config.sh
@@ -8,4 +8,4 @@ bundle=$(cat ${config_file} | jq -r '.bundle_url')
 driver=$(cat ${config_file} | jq -r '.driver_image')
 channel=$(cat ${config_file} | jq -r '.channel')
 
-oc_command="oc --insecure-skip-tls-verify"
+oc_command="oc --insecure-skip-tls-verify --kubeconfig /var/run/secrets/armsnokubeconfig"
